17179b03f5e9612e1ee46826ae7f2df359435d49
Abhay tomar <78420793+abhaytomar05@users.noreply.github.com>
2022-12-17 15:48:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function alertfunction() {
     var name = document.getElementById("name").value;
-    alert("Hlew " + name + "." + " Sorry to say, But we are working on it. Please try again later :)");
+    alert("Hlew " + name + "," + " Sorry to say, But we are working on it. Please try again later :)");
 }
 
